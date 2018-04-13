f8e66b2d6d13968e2c95277e24b4a84e870b853e
Pieter Delobelle <pieter.delobelle@gmail.com>
2018-04-13 14:12:48 +0200

    reverse engineered subject and public key

--- src/main/java/be/msec/Main.java
+++ src/main/java/be/msec/Main.java
@@ -5,13 +5,11 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-import sun.security.x509.X509CertImpl;
 
 import java.io.IOException;
+import java.nio.ByteBuffer;
 import java.security.*;
 import java.security.cert.CertificateException;
-import java.security.interfaces.RSAPrivateKey;
-import java.security.interfaces.RSAPublicKey;
 import java.util.Arrays;
 
 /**
@@ -26,25 +24,35 @@ public class Main extends Application
     {
         boolean PRINT_SHITTY_KEY = true;
 
-        if( PRINT_SHITTY_KEY )
+        if ( PRINT_SHITTY_KEY )
         {
             try
             {
                 //TODO: this is for anton
                 SSLUtil.createKeyStore( "TIME_keys.jks", "password" );
 
-                System.out.println("---info---");
-                byte[] info =  SSLUtil.getInfo("time");
-                System.out.println( Arrays.toString( info ) );
-                System.out.println("---signature---");
-                byte[] sign =  SSLUtil.getCertificate("time").getSignature();
-                System.out.println( Arrays.toString(  sign ) );
 
                 //RSAPublicKey key = (RSAPublicKey ) SSLUtil.getPublicKey();
                 //System.out.println( Arrays.toString( key.getPublicExponent().toByteArray() ) );
                 //System.out.println( Arrays.toString( key.getModulus().toByteArray() ) );
 
                 //System.out.println( key.getModulus().toByteArray().length );
+
+                System.out.println( "---info---" );
+                byte[] info = SSLUtil.getInfo( "time" );
+                System.out.println( Arrays.toString( info ) );
+                System.out.println( "---signature---" );
+                byte[] sign = SSLUtil.getCertificate( "time" ).getSignature();
+                System.out.println( Arrays.toString( sign ) );
+                System.out.println( "---key---" );
+                System.out.println( Arrays.toString( SSLUtil.getCertificate( "time" ).getPublicKey().getEncoded() ) );
+                System.out.println( "---name---" );
+                System.out.println( Arrays.toString( "time".getBytes() ) );
+                System.out.println( Arrays.toString( "CN=TIME".getBytes() ) );
+                System.out.println( Arrays.toString( "CN=ROOT".getBytes() ) );
+                System.out.println( Arrays.toString( "20180412090056".getBytes() ) );
+                System.out.println( Arrays.toString( "20180513090056".getBytes() ) );
+                System.out.println( Arrays.toString( ByteBuffer.allocate( Long.SIZE / Byte.SIZE ).putLong( 1526202056000L ).array() ) );
 
                 SSLUtil.createKeyStore( "CA.jks", "password" );
 
@@ -53,7 +61,7 @@ public class Main extends Application
                 Signature signature = Signature.getInstance( "SHA1withRSA" );
                 signature.initVerify( publicKey );
                 signature.update( info );
-                System.out.println(signature.verify( sign ));
+                System.out.println( signature.verify( sign ) );
                 //System.out.println( Arrays.toString( signature.sign() ) );
                 //System.out.println( signature.sign().length );
             }
@@ -89,13 +97,39 @@ public class Main extends Application
 
     }
 
+    public static byte[] getPublicKey(byte[] input, short length)
+    {
+        byte[] output = new byte[length];
+
+        for (short i = 0; i < length; i++)
+        {
+            output[i] = input[input.length - length + i ];
+        }
+
+        return output;
+    }
+
+    public static byte[] getSubjectName(byte[] input, short keyLength)
+    {
+        short count = 0;
+        short start = (short) (input.length - keyLength);
+        while (count++ != input[--start]);
+        byte[] output = new byte[count - 1];
+        for (short i = 0; i < count - 1; i++)
+        {
+            output[i] = input[ start + i + 1];
+        }
+
+        return output;
+    }
+
 
     @Override
     public void start( Stage stage ) throws Exception
     {
-        Parent root = FXMLLoader.load(getClass().getResource("/scene.fxml"));
-        stage.setTitle("Middleware");
-        stage.setScene(new Scene(root));
+        Parent root = FXMLLoader.load( getClass().getResource( "/scene.fxml" ) );
+        stage.setTitle( "Middleware" );
+        stage.setScene( new Scene( root ) );
         stage.show();
     }
 }
